45a34e15c77469f0f70530efc3c5cc761ca20fc6
Austinandrewvasquez <129329628+Austinandrewvasquez@users.noreply.github.com>
2023-09-10 17:30:43 -0600

    Add files via upload

--- file.py
+++ file.py
@@ -1,32 +1,31 @@
+class domainregistrar:
+    def__init__(self):
+    self.domain_database={}
 
-<!doctype html>
-<html lang="en" data-market="us">
-  <head>
-<link rel='preconnect dns-prefetch' href='https://triplewhale-pixel.web.app/' crossorigin />
-<link rel='preconnect dns-prefetch' href='https://api.config-security.com/' crossorigin />
-<script>
-/* >> TriplePixel :: start*/
-~function(W,H,A,L,E,_,B,N){function O(U,T,H,R){void 0===R&&(R=!1),H=new XMLHttpRequest,H.open("GET",U,!0),H.send(null),H.onreadystatechange=function(){4===H.readyState&&200===H.status?(R=H.responseText,U.includes(".txt")?eval(R):N[B]=R):(299<H.status||H.status<200)&&T&&!R&&(R=!0,O(U,T-1))}}if(N=window,!N[H+"sn"]){N[H+"sn"]=1;try{A.setItem(H,1+(0|A.getItem(H)||0)),(E=JSON.parse(A.getItem(H+"U")||"[]")).push(location.href),A.setItem(H+"U",JSON.stringify(E))}catch(e){}A.getItem('"!nC`')||(A=N,A[H]||(L=function(){return Date.now().toString(36)+"_"+Math.random().toString(36)},E=A[H]=function(t,e){return W=L(),(E._q=E._q||[]).push([W,t,e]),W},E.ch=W,B="configSecurityConfModel",N[B]=1,O("//conf.config-security.com/model",0),O("//triplewhale-pixel.web.app/triplefw.txt?",5)))}}("","TriplePixel",localStorage);
-/* << TriplePixel :: end*/
-</script>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+    def register_domain(self, domain_name, owner_name):
+        if domain_name in self.domain_database:
+            return "domain already registrered"
+        self.domain_database[domain_name] = owner_name
+       return f"domain{domain_name}registrered to{owner_name}"
 
-<link rel="preconnect" href="https://cdn.shopify.com" crossorigin>
-<link rel="preload" href="//testurldomain.com/cdn/shop/t/93/assets/bundle.css?v=154890970846882929411689697458" as="style">
-<link rel="preload" href="//testurldomain.com/cdn/shop/t/93/assets/bundle.js?v=151582954937367618501689697459" as="script">
+def check_domain(self, domain_name):
+    if domain_name in self.domain_database:
+        return f"domain{domain_name}is registered to"
+    {self.domain_database[domain_name]}"
+else:
+return "domain not found"
 
-
-<link rel="shortcut icon" href="https://" type="image/png" />
-
-<meta name="google-site-verification" content="MV8CebhWFyz8CRReIF79Mb1kMHsRbW_r-RRPRBvD0zA" />
-
-<title>coooooooode</title>
+def transfer_domain(self, domain_name, new_owner_name:)
+if domain_name in self.domain_database:
+    self.domain_database[domain_name] = new_owner_name
+else:
+return "domain not found"
 
 
-  <meta name="description" content="the test site of my code">
+# Example usage
+registrar = domainregistrar()
 
-
-
-  <link rel="canonical" href="https://testurldomain.com/">
+print(registrar.register_domain("example.com","john doe"))
+print(registrar.check_domain("example.com"))
+print(registrar.transfer_domain("example.com","jane smith"))
+print(registrar.check_domain("example.com"))
